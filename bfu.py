5535891f253cd6dae79673f10a1c4b26a0fa61aa
maheshbfu <63104552+maheshbfu@users.noreply.github.com>
2020-04-05 15:13:39 +0200

    Update bfu.py

--- bfu.py
+++ bfu.py
@@ -1,24 +1,39 @@
-import openpyxl as xl; 
-df = pd.read_excel (r'D:\BothOfUs\pytoh\Book1.xlsx')
-print (df)
-filename ="D:\BothOfUs\pytoh\Book1.xlsx"
-wb1 = xl.load_workbook(filename) 
-ws1 = wb1.worksheets[0] 
-mr = ws1.max_row 
-mc = ws1.max_column 
-for i in range (1, mr + 1): 
-    for j in range (1, mc + 1):  
-        c = ws1.cell(row = i, column = j)        
-message=input()
-poolurl="https://www.google.com"
-def bot(message):
-    if "/pool" in message:
-            message=message.split(" ")
-            del message[0]
-            poolname=message
-            text="Please! Give more details of the pool"
-            print(poolname,text)
-            message=input()
-            pooldes=message
-            print("Pool",poolname,"has been created - ",pooldes)
-            print("Your pool is almost ready: Go to your specific pool page on, ",poolurl,"to add a catalogue and see the pool order recap.")
+import os, slackclient
+
+SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
+SLACK_USER_TOKEN = os.environ.get('SLACK_USER_TOKEN')
+
+# initialize slack client
+SolidarityFood_client = slackclient.SlackClient(SLACK_BOT_TOKEN)
+fsc = slackclient.SlackClient(SLACK_USER_TOKEN)
+
+# check if everything is alright
+print(SLACK_BOT_TOKEN)
+print('Dayo')
+is_ok = SolidarityFood_client.api_call("users.list").get('ok')
+mem = SolidarityFood_client.api_call("users.list").get('members')
+if(is_ok):
+    for user in mem:
+        
+        usName = user.get('name')
+        if (usName == 'food_bot_ish'):
+            usId = user.get('id')
+            print(usName, usId)
+print(is_ok)
+
+#SolidarityFood_client.api_call("chat.postMessage", channel="food_test", text="Dayo is the best")
+#SolidarityFood_client.api_call("chat.postMessage", channel="general", text=" Thank you!")
+history = fsc.api_call("groups.history", channel="GARQ9DUKF")
+for message in history['messages']:
+    print(message)
+    if '/pool' in message:
+        message=message.split(" ")
+        del message[0]
+        poolname=message
+        text="Please! Give more details of the pool"
+            SolidarityFood_client.api_call("chat.postMessage", channel="food_test", text=(poolname,text))
+
+    #need to assign to the next coming message
+    pooldes=message
+    SolidarityFood_client.api_call("chat.postMessage", channel="food_test", text=("Pool",poolname,"has been created - ",pooldes))
+    SolidarityFood_client.api_call("chat.postMessage", channel="food_test", text=(("Your pool is almost ready: Go to your specific pool page on, ",poolurl,"to add a catalogue and see the pool order recap."))
